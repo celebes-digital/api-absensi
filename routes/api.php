e9d64cbca0e5314fda9eb5ee04c6ecd90929b7bd
perdidev <perdidev@gmail.com>
2024-10-03 14:00:08 +0800

    fix/error: solve user 403 forbidden becuase middleware

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,13 @@ Route::middleware('auth:sanctum', 'isEmailVerify')->group(function () {
     Route::get('user/shift',            [ShiftKerjaController::class,   'getShiftKerja']);
     Route::get('user/kehadiran',        [KehadiranController::class,    'getKehadiran']);
     Route::get('kehadiran/get-kode',    [KehadiranController::class,    'generateKeyAbsensi']);
+    Route::apiResource('user',          UserController::class);
 
     Route::middleware('admin')->group(function () {
         Route::post('kehadiran/confirm',        [KehadiranController::class,    'confirmAbsensi']);
         Route::post('email/send-verification',  [EmailController::class,        'sendEmailVerification']);
         
         Route::apiResource('gaji',          GajiController::class);
-        Route::apiResource('user',          UserController::class);
         Route::apiResource('pegawai',       PegawaiController::class);
         Route::apiResource('payroll',       PayrollController::class);
         Route::apiResource('kehadiran',     KehadiranController::class);
